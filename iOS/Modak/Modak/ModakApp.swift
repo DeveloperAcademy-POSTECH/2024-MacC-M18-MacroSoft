661f23b99755f5ab896818fc58cac7960549345f
jihee-daily <kim76955@gmail.com>
2024-10-02 09:44:21 +0900

    init

---
+++ iOS/Modak/Modak/ModakApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModakApp.swift
+//  Modak
+//
+//  Created by kimjihee on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ModakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
