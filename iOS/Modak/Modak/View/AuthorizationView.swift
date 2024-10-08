e779f624035851c25b5713337c014767036f5042
jihee-daily <kim76955@gmail.com>
2024-10-08 10:02:32 +0900

    feat: #6 add and delete

---
+++ iOS/Modak/Modak/View/AuthorizationView.swift
@@ -0,0 +1,23 @@
+//
+//  Authorization.swift
+//  Modak
+//
+//  Created by kimjihee on 10/8/24.
+//
+
+import SwiftUI
+
+struct AuthorizationView: View {
+    var body: some View {
+        ZStack {
+            Color.backgroundDefault.ignoresSafeArea(.all)
+            VStack {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    AuthorizationView()
+}
