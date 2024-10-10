820423808fb767c4a20010d88583eed70a807d05
jihee-daily <kim76955@gmail.com>
2024-10-10 23:51:42 +0900

    feat: #12 add DBSCAN Algorithm

--- iOS/Modak/Modak/Model/DBSCAN.swift
+++ iOS/Modak/Modak/Model/DBSCAN.swift
@@ -5,14 +5,76 @@
 //  Created by kimjihee on 10/10/24.
 //
 
-import SwiftUI
+import Foundation
 
-struct DBSCAN: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class DBSCAN {
+    var eps: TimeInterval // 최대 시간 차이 (초 단위)
+    var minPts: Int // 최소 포인트 수
+
+    init(eps: TimeInterval = 10800, minPts: Int = 10) { // 기본값 설정
+        self.eps = eps
+        self.minPts = minPts
     }
-}
+    
+    func changeMinPts(_ minPts: String) {
+        self.minPts = Int(minPts) ?? 10
+    }
+    
+    func changeMaxDis(_ maxDis: String) {
+        self.eps = TimeInterval(maxDis) ?? 10800
+    }
 
-#Preview {
-    DBSCAN()
+    func applyAlgorithm(points: [PhotoMetadata], progressUpdate: (Int) -> Void) -> [[PhotoMetadata]] {
+        var clusters: [[PhotoMetadata]] = []
+        var visited = Set<PhotoMetadata>()
+        var noise: [PhotoMetadata] = []
+        var processedPoints = 0
+        
+        func regionQuery(point: PhotoMetadata) -> [PhotoMetadata] {
+            return points.filter { timeDifference($0.creationDate, point.creationDate) < eps }
+        }
+        
+        func expandCluster(point: PhotoMetadata, neighbors: [PhotoMetadata], cluster: inout [PhotoMetadata]) {
+            cluster.append(point)
+            var queue = neighbors
+            while !queue.isEmpty {
+                let neighbor = queue.removeFirst()
+                if !visited.contains(neighbor) {
+                    visited.insert(neighbor)
+                    let newNeighbors = regionQuery(point: neighbor)
+                    if newNeighbors.count >= minPts {
+                        queue.append(contentsOf: newNeighbors)
+                    }
+                }
+                if !cluster.contains(neighbor) {
+                    cluster.append(neighbor)
+                }
+            }
+        }
+        
+        for point in points {
+            if visited.contains(point) { continue }
+            visited.insert(point)
+            let neighbors = regionQuery(point: point)
+            if neighbors.count < minPts {
+                noise.append(point)
+            } else {
+                var cluster: [PhotoMetadata] = []
+                expandCluster(point: point, neighbors: neighbors, cluster: &cluster)
+                clusters.append(cluster)
+            }
+            
+            // 진행 상황 업데이트
+            processedPoints += 1
+            progressUpdate(processedPoints) // 현재 처리된 포인트 수를 콜백으로 반환
+        }
+        
+        return clusters
+    }
+    
+    // 시간 간의 차이 계산
+    private func timeDifference(_ date1: Date?, _ date2: Date?) -> TimeInterval {
+        guard let date1 = date1, let date2 = date2 else { return Double.infinity }
+        return abs(date1.timeIntervalSince(date2))
+    }
 }
