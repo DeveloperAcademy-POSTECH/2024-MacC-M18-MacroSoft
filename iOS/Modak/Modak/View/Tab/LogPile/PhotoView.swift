65de181dc9e1772c8579cff4b45503a2bca260a3
Arthur <ays99111@naver.com>
2024-10-14 22:38:22 +0900

    feat: #26 LogPile 플로우 뷰 폴더링

---
+++ iOS/Modak/Modak/View/Tab/LogPile/PhotoView.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoView.swift
+//  Modak
+//
+//  Created by Park Junwoo on 10/14/24.
+//
+
+import SwiftUI
+
+struct PhotoView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
