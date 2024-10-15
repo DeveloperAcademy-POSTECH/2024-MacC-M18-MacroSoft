08176f592a5cf38482089e8c07955261cb99402c
Arthur <ays99111@naver.com>
2024-10-16 04:38:54 +0900

    feat: #29 CampfireView 임시 UI 구현

--- iOS/Modak/Modak/View/Tab/CampfireView.swift
+++ iOS/Modak/Modak/View/Tab/CampfireView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct CampfireView: View {
+    // TODO: 캠프파이어 임시뷰
     var body: some View {
-        Text("CampfireView")
+        ZStack {
+            Color.backgroundLogPile.ignoresSafeArea()
+            LinearGradient.profileViewBackground.ignoresSafeArea()
+            
+            VStack(spacing: 8) {
+                Spacer()
+                Text("캠핑장이 곧 개장할 예정이에요")
+                    .foregroundStyle(.textColorGray1)
+                    .font(
+                        Font.custom("Pretendard-Bold", size: 22)
+                    )
+                Text("안전하게 마무리될 때까지 기다려주세요")
+                    .foregroundStyle(.textColorGray2)
+                    .font(
+                        Font.custom("Pretendard-Regular", size: 16)
+                    )
+                
+                Spacer()
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                NavigationLink {
+                    AuthorizationView()
+                } label: {
+                    Image(systemName: "plus")
+                        .foregroundStyle(.textColorTitleView)
+                }
+
+                
+            }
+        }
     }
 }
 
