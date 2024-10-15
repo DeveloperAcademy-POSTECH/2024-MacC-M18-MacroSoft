6216b3144ce9b695f1dfbe7fc9c9849a2bc27269
jihee-daily <kim76955@gmail.com>
2024-10-15 12:09:58 +0900

    feat: #27 get account info

--- iOS/Modak/Modak/View/RegisterView.swift
+++ iOS/Modak/Modak/View/RegisterView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import AuthenticationServices
+import CryptoKit
 
 struct RegisterView: View {
     @AppStorage("isSkipRegister") var isSkipRegister: Bool = false
@@ -61,10 +62,29 @@ struct RegisterView: View {
                   onCompletion: { result in
                     // 인증 결과 처리
                     switch result {
-                        case .success(let authorization):
-                            print("Authorization successful: \(authorization)")
-                      case .failure(let error):
-                        print("Authorization failed: \(error.localizedDescription)")
+                    case .success(let authResults):
+                        print("Apple Login Successful")
+                        switch authResults.credential{
+                            case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                                // 계정 정보 가져오기
+                                let authorizationCode = String(data: appleIDCredential.authorizationCode!, encoding: .utf8)
+                                let identityToken = String(data: appleIDCredential.identityToken!, encoding: .utf8)
+                                let userIdentifier = appleIDCredential.user
+                                let encryptedUserIdentifier = SHA256.hash(data: Data(userIdentifier.utf8)).compactMap { String(format: "%02x", $0) }.joined()
+                                
+                                // 값 확인
+                                print("Authorization Code: \(authorizationCode ?? "")")
+                                print("Identity Token: \(identityToken ?? "")")
+                                print("Encrypted User Identifier: \(encryptedUserIdentifier)")
+                                
+                                // 서버에 값 전송
+                                                
+                            default:
+                                break
+                        }
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                        print("error")
                     }
                   }
                 )
