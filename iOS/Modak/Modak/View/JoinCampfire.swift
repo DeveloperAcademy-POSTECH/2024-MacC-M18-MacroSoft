d6615d3106bdd6956a25f30b766341335d51eac8
jihee-daily <kim76955@gmail.com>
2024-10-30 04:04:21 +0900

    ref: #48 view separation

--- iOS/Modak/Modak/View/JoinCampfire.swift
+++ iOS/Modak/Modak/View/JoinCampfire.swift
@@ -10,21 +10,73 @@ import SwiftUI
 struct JoinCampfire: View {
     @State var roomName:  String = ""
     @State var roomPassword: String = ""
-    @State private var isCameraMode: Bool = true
+    @State private var isCameraMode: Bool = false
     
     var body: some View {
-        ZStack(alignment: .leading) {
+        ZStack {
             Color.backgroundDefault.ignoresSafeArea(.all)
             
+            contentView
+            
+            // 모드 선택 토글 버튼
             VStack {
-                Text("모닥불로 가는 길을 알려주세요")
-                    .foregroundStyle(Color.white)
-                    .font(.custom("Pretendard-Bold", size: 18))
-                    .lineSpacing(18 * 0.45)
-                    .padding(EdgeInsets(top: 8, leading: 22, bottom: 0, trailing: 0))
-                
                 Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        isCameraMode.toggle() // 모드 전환
+                    }) {
+                        Text(isCameraMode ? "촬영해서 입력" : "직접 입력하기")
+                            .font(.custom("Pretendard-Regular", size: 16))
+                            .foregroundColor(.white)
+                            .padding(EdgeInsets(top: 14, leading: 20, bottom: 14, trailing: 20))
+                            .background(Color.textBackgroundRedGray.opacity(0.1))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 100)
+                                    .stroke(Color.mainColor1, lineWidth: 1.8) // 라인선 추가
+                            )
+                            .clipShape(RoundedRectangle(cornerRadius: 100))
+                    }
+                    .padding(.trailing, 20)
+                }
+                .padding(.bottom, 14)
             }
+            
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                BackButton()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private var contentView: some View {
+        if isCameraMode {
+            cameraModeView
+        } else {
+            manualEntryView
+        }
+    }
+    
+    private var cameraModeView: some View {
+        VStack {
+            Text("카메라뷰")
+        }
+    }
+    
+    private var manualEntryView: some View {
+        VStack(alignment: .leading) {
+            Text("모닥불로 가는 길을 알려주세요")
+                .foregroundStyle(Color.white)
+                .font(.custom("Pretendard-Bold", size: 18))
+                .lineSpacing(18 * 0.45)
+                .padding(EdgeInsets(top: 8, leading: 22, bottom: 0, trailing: 0))
+            
+            Spacer()
             
             GeometryReader { geometry in
                 VStack {
@@ -63,40 +115,6 @@ struct JoinCampfire: View {
                     
                     Spacer()
                 }
-            }
-            
-            // 모드 선택 토글 버튼
-            VStack {
-                Spacer()
-                
-                HStack {
-                    Spacer()
-                    
-                    Button(action: {
-                        isCameraMode.toggle() // 모드 전환
-                    }) {
-                        Text(isCameraMode ? "촬영해서 입력" : "직접 입력하기")
-                            .font(.custom("Pretendard-Regular", size: 16))
-                            .foregroundColor(.white)
-                            .padding(EdgeInsets(top: 14, leading: 20, bottom: 14, trailing: 20))
-                            .background(Color.textBackgroundRedGray.opacity(0.1))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 100)
-                                    .stroke(Color.mainColor1, lineWidth: 1.8) // 라인선 추가
-                            )
-                            .clipShape(RoundedRectangle(cornerRadius: 100))
-                            .animation(.easeInOut, value: isCameraMode)
-                    }
-                    .padding(.trailing, 20)
-                }
-                .padding(.bottom, 14)
-            }
-            
-        }
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .topBarLeading) {
-                BackButton()
             }
         }
     }
