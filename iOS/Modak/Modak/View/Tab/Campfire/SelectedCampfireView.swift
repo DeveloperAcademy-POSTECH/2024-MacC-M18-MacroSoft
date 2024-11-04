80841bb56363f21a8b5b4912450177c47cc88f22
Arthur <ays99111@naver.com>
2024-11-04 12:08:54 +0900

    feat: #49 오늘의 사진 크기 조정 코드 수정

--- iOS/Modak/Modak/View/Tab/Campfire/SelectedCampfireView.swift
+++ iOS/Modak/Modak/View/Tab/Campfire/SelectedCampfireView.swift
@@ -11,7 +11,7 @@ struct SelectedCampfireView: View {
     // TODO: 참여한 모닥불의 로그가 없는지 체크하는 로직 추가
     @State private var isEmptyCampfireLog: Bool = true
     // TODO: 참여한 모닥불의 이름 가져오는 로직 추가
-    @State private var campfireName: String = "macrosoft"
+    @State private var campfireName: String = "MacroSoft"
     
     var body: some View {
         CampfireViewTopButton(campfireName: $campfireName)
@@ -89,15 +89,19 @@ private struct CampfireViewTodayPhoto: View {
     @State private var randomRotation: Bool = Bool.random()
     @State private var isTodayPhotoFullSheet: Bool = false
     
+    // TODO: 오늘의 사진 높이가 320 넘는지 판별하는 로직 추가하기
+    private var isTodayPhotoHeightOver320: Bool = false
+    
     var body: some View {
         VStack(spacing: 8) {
             Button {
                 isTodayPhotoFullSheet = true
             } label: {
-                Image(.logs)
+                Image(.photosIcon)
                     .resizable()
-                
+                    .scaledToFit()
                     .clipShape(RoundedRectangle(cornerRadius: 14))
+                    .padding(.init(top: 8, leading: 8, bottom: 0, trailing: 8))
             }
             
             Text("오늘의 사진")
@@ -111,8 +115,8 @@ private struct CampfireViewTodayPhoto: View {
                 .stroke(Color.init(hex: "6E615F").opacity(0.32), lineWidth: 1)
         }
         .shadow(color: .black.opacity(0.25), radius: 9.09486, x: 0, y: 4.54743)
-        .frame(height: UIScreen.main.bounds.height / 3)
-        .padding(.horizontal, 24)
+        // TODO: 오늘의 사진 높이가 320 넘는지 판별하는 로직 추가하기
+        .frame(width: 320, height: isTodayPhotoHeightOver320 ? 320 : .infinity)
         .rotationEffect(.degrees(randomRotation ? 2 : -2))
         .fullScreenCover(isPresented: $isTodayPhotoFullSheet) {
             TodayPhotoFullSheet()
@@ -256,7 +260,6 @@ private struct CampfireViewEmptyLogView: View {
             }
             .padding(.horizontal, 70)
             
-
             Spacer()
         }
     }
