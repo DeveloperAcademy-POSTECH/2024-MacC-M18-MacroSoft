10e3e62ebd442df7f8b9603240ba6d52a87f3e75
Arthur <ays99111@naver.com>
2024-11-07 11:37:55 +0900

    feat: #57 CampfireLogPileView 파일 생성

---
+++ iOS/Modak/Modak/View/Tab/Campfire/CampfireLogPileView.swift
@@ -0,0 +1,18 @@
+//
+//  CampfireLogPileView.swift
+//  Modak
+//
+//  Created by Park Junwoo on 11/7/24.
+//
+
+import SwiftUI
+
+struct CampfireLogPileView: View {
+    var body: some View {
+        Text("CampfireLogPileView")
+    }
+}
+
+#Preview{
+    CampfireLogPileView()
+}
