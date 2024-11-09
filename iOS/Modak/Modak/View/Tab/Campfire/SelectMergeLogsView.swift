fa34d9ec7394336574d0e10c160c832491725c6d
Arthur <ays99111@naver.com>
2024-11-09 23:17:13 +0900

    feat: #61 SelectMergeLogs 뷰 파일 생성

---
+++ iOS/Modak/Modak/View/Tab/Campfire/SelectMergeLogsView.swift
@@ -0,0 +1,14 @@
+//
+//  SelectMergeLogsView.swift
+//  Modak
+//
+//  Created by Park Junwoo on 11/9/24.
+//
+
+import SwiftUI
+
+struct SelectMergeLogsView: View {
+    var body: some View {
+        Text("SelectMergeLogsView")
+    }
+}
