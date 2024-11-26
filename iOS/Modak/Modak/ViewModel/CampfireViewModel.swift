da165cdca03f445cba0a22fa88090c2773e72c89
Arthur <ays99111@naver.com>
2024-11-26 15:12:14 +0900

    feat: #73 CampfireViewModel 수정

--- iOS/Modak/Modak/ViewModel/CampfireViewModel.swift
+++ iOS/Modak/Modak/ViewModel/CampfireViewModel.swift
@@ -7,16 +7,171 @@
 
 import SwiftUI
 import Combine
+import SDWebImageWebPCoder
 
 class CampfireViewModel: ObservableObject {
-    @Published var campfire: Campfire? // 단일 데이터
-    @Published var campfires: [Campfire] = [] // 데이터 묶음
+    @Published var mainCampfireInfo: MainCampfireInfo? // 단일 데이터
+    @Published var myCampfireInfos: [CampfireInfo] = [] // 데이터 묶음
+    
+    // TODO: 제거하기
+    @Published var currentCampfire: Campfire? // 단일 데이터
+    // TODO: 제거하기
+    @Published var myCampfires: [Campfire] = [] // 데이터 묶음
+    // TODO: 제거하기
     @Published var isEmptyCampfire: Bool = true
-    @Published var showNetworkAlert: Bool = false
+    // TODO: 제거하기
     @AppStorage("recentVisitedCampfirePin") var recentVisitedCampfirePin: Int = 0
     
-    init() {
-        fetchCampfireMainInfo()
+    @Published var showNetworkAlert: Bool = false
+    @Published var currentCampfirePin: Int = 0
+    @Published var mainTodayImage: UIImage?
+    @Published var currentCampfireYearlyLogs: [(MonthlyLogsOverview)] = []
+    @Published var currentCampfireLogImages: [CampfireLogImage] = []
+    @Published var currentCampfireLogImageDetail: CampfireLogImageDetailResult?
+    
+    //    init() {
+    //        fetchCampfireMainInfo()
+    //    }
+    
+    @MainActor
+    func testChangeCurrentCampfirePin(_ pin: Int) async {
+        self.currentCampfirePin = pin
+    }
+    
+    func testFetchCampfireInfos() async {
+        do {
+            let response = try await NetworkManager.shared.requestRawData(router: .getMyCampfires)
+            let result = try JSONDecoder().decode(ResponseModel<CampfireLogsInfosResult>.self, from: response).result
+            
+            DispatchQueue.main.async {
+                self.myCampfireInfos = result.campfireInfos
+                if self.currentCampfirePin == 0 {
+                    self.currentCampfirePin = result.campfireInfos.first?.campfirePin ?? 0
+                }
+            }
+            
+        } catch {
+            print("testfetchCampfireInfos error: \(error)")
+        }
+    }
+    
+    func testFetchMainCampfireInfo() async {
+        do {
+            if currentCampfirePin != 0 {
+                let response = try await NetworkManager.shared.requestRawData(router: .getCampfireMainInfo(campfirePin: currentCampfirePin))
+                
+                let result = try JSONDecoder().decode(ResponseModel<MainCampfireInfo>.self, from: response).result
+                DispatchQueue.main.async {
+                    self.mainCampfireInfo = result
+                    
+                }
+            } else {
+                print("testfetchCampfireMainInfo currentCampfirePin == 0")
+            }
+        } catch {
+            print("testfetchCampfireMainInfo error: \(error)")
+        }
+    }
+    
+    func testCreateCampfire(newCampfireName: String) async {
+        do {
+            let response = try await NetworkManager.shared.requestRawData(router: .createCampfire(campfireName: newCampfireName))
+            
+            let result = try JSONDecoder().decode(ResponseModel<[String: Int]>.self, from: response)
+            
+            if let newCampfirePin = result.result.first?.value {
+                await testChangeCurrentCampfirePin(newCampfirePin)
+            }
+            
+            print("testCreateCampfire result: \(result)")
+            
+            Task {
+                await testFetchCampfireInfos()
+                await testFetchMainCampfireInfo()
+                DispatchQueue.main.async {
+                    self.mainTodayImage = nil
+                }
+            }
+        } catch {
+            print("testCreateCampfire error: \(error)")
+        }
+    }
+    
+    @MainActor
+    func testFetchCampfireLogsPreview() async {
+        do {
+            let response = try await NetworkManager.shared.requestRawData(router: .getCampfireLogsPreview(campfirePin: currentCampfirePin))
+            
+            let result = try JSONDecoder().decode(ResponseModel<CampfireLogsPreviewResult>.self, from: response).result
+            
+            if !result.logOverviews.isEmpty {
+                // logs: [Log]를, monthlyLogs(MonthlyLogs Model과는 다름): [Date : [Log]] 형태로 (년, 월 기준으로)
+                let monthlyLogs = Dictionary(grouping: result.logOverviews) { log in
+                    let components = Calendar.current.dateComponents([.year, .month], from: log.startAt.iso8601ToDate)
+                    return Calendar.current.date(from: components)!
+                }
+                
+                // YearlyLogs를 최신 순으로 정렬하고, 각 월 안에서 다시 일별로 그룹화
+                currentCampfireYearlyLogs = monthlyLogs.sorted { $0.key > $1.key } // 최신 월 순으로 정렬
+                    .map { (month, logsInMonth) in
+                        // 같은 월 안에서 다시 일별로 그룹화
+                        let dailyLogs = Dictionary(grouping: logsInMonth) { log in
+                            Calendar.current.startOfDay(for: log.startAt.iso8601ToDate) // 일 단위로 그룹화
+                        }
+                        
+                        // 날짜별 로그들을 최신 날짜 순으로 정렬
+                        let sortedDailyLogs = dailyLogs.sorted { $0.key > $1.key }
+                            .map { (day, logsInDay) in
+                                DailyLogsOverview(date: day, logs: logsInDay.sorted { $0.startAt.iso8601ToDate > $1.startAt.iso8601ToDate }) // 시간순으로 정렬
+                            }
+                        
+                        // 월 단위로 묶고, 그 안에 일별로 묶인 로그들 포함
+                        return MonthlyLogsOverview(date: month, dailyLogs: sortedDailyLogs)
+                    }
+            }
+        } catch {
+            print("testFetchCampfireLogs error: \(error)")
+        }
+    }
+    
+    @MainActor
+    func getCampfireLogImages(logId: Int) async {
+        do {
+            let response = try await NetworkManager.shared.requestRawData(router: .getCampfireLogImages(campfirePin: currentCampfirePin, logId: logId))
+            
+            let result = try JSONDecoder().decode(ResponseModel<CampfireLogImagesResult>.self, from: response).result
+            
+            currentCampfireLogImages = result.images
+        } catch {
+            print("getCampfireLogImages error: \(error)")
+        }
+    }
+    
+    @MainActor
+    func getCampfireLogImageDetail(imageId: Int) async {
+        do {
+            let response = try await NetworkManager.shared.requestRawData(router: .getCampfireLogImageDetail(campfirePin: currentCampfirePin, imageId: imageId))
+            
+            let result = try JSONDecoder().decode(ResponseModel<CampfireLogImageDetailResult>.self, from: response).result
+            
+            currentCampfireLogImageDetail = result
+        } catch {
+            print("getCampfireLogImageDetail error: \(error)")
+        }
+    }
+    
+    func updateCampfireName(newName: String) async {
+        do {
+            let response = try await NetworkManager.shared.requestRawData(router: .updateCampfireName(campfirePin: currentCampfirePin, parameters: ["newCampfireName": newName]))
+            print("updateCampfireName success: \(response)")
+            
+            Task {
+                await testFetchCampfireInfos()
+                await testFetchMainCampfireInfo()
+            }
+        } catch {
+            print("updateCampfireName error: \(error)")
+        }
     }
     
     func fetchCampfireMainInfo() {
@@ -41,7 +196,7 @@ class CampfireViewModel: ObservableObject {
                     let campfire = try JSONDecoder().decode(Campfire.self, from: jsonData)
                     
                     DispatchQueue.main.async {
-                        self.campfire = campfire
+                        self.currentCampfire = campfire
                         self.isEmptyCampfire = false
                         self.recentVisitedCampfirePin = campfire.pin
                     }
@@ -78,8 +233,8 @@ class CampfireViewModel: ObservableObject {
                     }
                     
                     DispatchQueue.main.async {
-                        self.campfires = fetchedCampfires
-                        self.campfire = fetchedCampfires.first(where: { $0.pin == self.recentVisitedCampfirePin }) ?? fetchedCampfires.first
+                        self.myCampfires = fetchedCampfires
+                        self.currentCampfire = fetchedCampfires.first(where: { $0.pin == self.recentVisitedCampfirePin }) ?? fetchedCampfires.first
                         self.isEmptyCampfire = fetchedCampfires.isEmpty
                         
                         // 콜백으로 데이터를 반환
@@ -141,6 +296,73 @@ class CampfireViewModel: ObservableObject {
     func updateRecentVisitedCampfirePin(to pin: Int) {
         recentVisitedCampfirePin = pin
         fetchCampfireMainInfo()  // 새로운 pin에 맞는 캠프파이어 정보 가져오기
+    }
+    
+    func getWebpImageDataURL(imageURLName: String) async -> URL? {
+        do {
+            let baseURL = Bundle.main.environmentVariable(forKey: "ImageURL")
+            let totalURL = baseURL! + "/" + imageURLName
+            if let url = URL(string: totalURL) {
+                return url
+            } else {
+                throw NetworkError.invalidURL
+            }
+        } catch {
+            print("getWebpImageDataURL Error: \(error)")
+            return nil
+        }
+    }
+    
+    func getWebpImageData(imageURLName: String) async -> Data? {
+        do {
+            let baseURL = Bundle.main.environmentVariable(forKey: "ImageURL")
+            let totalURL = baseURL! + "/" + imageURLName
+            if let url = URL(string: totalURL) {
+                let response = try await URLSession.shared.data(from: url)
+                let imageData = response.0
+                return imageData
+            } else {
+                throw NetworkError.invalidURL
+            }
+        } catch {
+            print("getWebpImageData Error: \(error)")
+            return nil
+        }
+    }
+    
+    private func convertWebpDataToUIImage(webpData: Data?) -> UIImage? {
+        if let data = webpData {
+            let uiImage = SDImageWebPCoder.shared.decodedImage(with: data)
+            return uiImage
+        } else {
+            return nil
+        }
+    }
+    
+    func fetchTodayImage(imageURLName: String) async -> UIImage? {
+        if imageURLName != ""  {
+            let webpImageData = await getWebpImageData(imageURLName: imageURLName)
+            return convertWebpDataToUIImage(webpData: webpImageData)
+        } else {
+            print("fetchTodayImage imageURLName is empty")
+            return nil
+        }
+    }
+    
+    func updateTodayImageEmotion(emotion: String) async {
+        if let campfire = mainCampfireInfo {
+            do {
+                let response = try await NetworkManager.shared.requestRawData(router: .updateTodayImageEmotion(campfirePin: campfire.campfirePin, imageId: campfire.todayImage.imageId, parameters: ["emotion" : emotion]))
+                
+                let result = try JSONDecoder().decode(ResponseModel<CampfireTodayImageEmotionResult>.self, from: response)
+                
+                print("updateTodayImageEmotion result:\n\(result)")
+            } catch {
+                print("updateTodayImageEmotion error:\n\(error)")
+            }
+        } else {
+            print("updateTodayImageEmotion mainCampfireInfo nil error")
+        }
     }
 }
 
